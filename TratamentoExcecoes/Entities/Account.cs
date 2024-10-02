419c48bcfe815567068b91897101b4bab2e1b671
Cassiano Baldasso <baldassocassiano@gmail.com>
2024-10-01 22:52:50 -0300

    Primeira versão pronta da tarefa

---
+++ TratamentoExcecoes/Entities/Account.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Globalization;
+
+namespace TratamentoExcecoes.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() { }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit (double amount) 
+        {
+            if(amount >= 0)
+            {
+                Balance += amount;
+                Console.Write("New balance: " + Balance.ToString("F2", CultureInfo.InvariantCulture));
+                return;
+            }
+            else
+            {
+                throw new Exception("Deposit error: Invalid value");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(amount < WithdrawLimit && amount >= 0 && amount < Balance)
+            {
+                Balance -= amount;
+                Console.Write("New balance: " + Balance.ToString("F2", CultureInfo.InvariantCulture));
+                return;
+            }
+            else if(amount > Balance)
+            {
+                throw new Exception("Withdraw error: not enough balance");
+            }
+            else if (amount < 0)
+            {
+                throw new Exception("Withdraw error: negative withdraw");
+            }
+            else
+            {
+                throw new Exception("Withdraw error: The amount exceeds withdraw limit");
+            }
+
+        }
+
+    }
+}
---
+++ TratamentoExcecoes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Numerics;
+using System.Globalization;
+using TratamentoExcecoes.Entities;
+using System.Security.Principal;
+
+namespace TratamentoExcecoes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine("");
+
+            try
+            {
+                Console.Write("Do you want to deposit or withdraw?: d(to deposit) or w(to withdraw) ");
+                string operation = Console.ReadLine();
+
+                if (operation == "w")
+                {
+                    Console.Write("Enter amount for withdraw: ");
+                    double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    account.Withdraw(withdraw);
+                }
+                else if(operation == "d")
+                {
+                    Console.Write("Enter amount for deposit: ");
+                    double deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    account.Deposit(deposit);
+                }
+                else
+                {
+                    Console.WriteLine("Operation invalid");
+                }
+
+                Console.WriteLine("");
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+
+}
